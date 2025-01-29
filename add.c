f4aa82a97c5c461a81d7fd235679aee785b760f6
Prajktadesai31 <126471578+Prajktadesai31@users.noreply.github.com>
2025-01-29 15:19:24 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+//write a program to add two numbers
+#include<stdio.h>
+int main()
+{
+   int a,b; //declaration.
+   printf(" \n  Enter a and b value");
+   scanf("%d%d",&a,&b);
+    
+   printf("\n addition=%d",a+b);
+  
+ return 0;
+}
+
+
+
